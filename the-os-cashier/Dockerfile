ef62c5e41e48be0eb7fb932c28a2e77ca4336f93
Aditya GCloud <ag15035@gmail.com>
2021-07-25 18:48:24 +0000

    Reduced image size; Dockercompose working; Added plan for client code

--- the-os-cashier/Dockerfile
+++ the-os-cashier/Dockerfile
@@ -1,27 +1,20 @@
-FROM hyperledger/sawtooth-shell:nightly
+# This is a multi-stage dockerfile, to reduce final image size
+FROM rust:1.53 AS BUILD_STAGE
 
 RUN apt update
 RUN apt install gcc curl libzmq3-dev libssl-dev pkg-config protobuf-compiler -y
 
-# install rustup
-RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o /tmp/rustup.sh
-RUN chmod 744 /tmp/rustup.sh
-RUN /tmp/rustup.sh -y
-
 # Copy & Build the "Client" & "Transaction processor"
 COPY client /tmp/client
 WORKDIR /tmp/client
-RUN $HOME/.cargo/bin/cargo build --release
-RUN cp ./target/release/os-cashier-cli /usr/bin
+RUN cargo build --release
 
 COPY processor /tmp/processor
 WORKDIR /tmp/processor
-RUN $HOME/.cargo/bin/cargo build --release
-RUN cp ./target/release/os-cashier-tp /usr/bin
+RUN cargo build --release
 
-# Remove source code & rustup,cargo
-RUN rm -rf /tmp/{rustup.sh,client,processor}
-RUN $HOME/.cargo/bin/rustup self uninstall -y
-WORKDIR /
+FROM hyperledger/sawtooth-shell:nightly
+
+COPY --from=BUILD_STAGE /tmp/client/target/release/os-cashier-cli /tmp/processor/target/release/os-cashier-tp /usr/bin/
 
 CMD bash
--- the-os-cashier/client/src/main.rs
+++ the-os-cashier/client/src/main.rs
@@ -30,4 +30,37 @@ fn main() {
                         (@arg module: +required "Name of pre-available module")
                      )
                     ).get_matches();
+
+    /* TODO: Instead of try catch block, in the function that interacts with the API, call
+     * process::exit there
+     * Later, if needed create a try catch block here
+     */
+
+    
+    /* TODO: Step 1- Create a Client object, and call respective function for the operation, for
+     * eg. reg, list etc.
+     */
+
+
+    /* TODO: Step 2- Inside those function, make a call to "_send_transaction" that creates the
+     * Transaction, Header, Payload objects (and serialise this payload object), and finally make
+     * the REST API call, for now lets say that's "send_rest_api_call"
+     */
+
+
+    /* TODO: Step 3- Inside _send_transaction itself, batch all transactions, say with a
+     * "create_batch_list([transaction])" function, then this batch list will be serialised to a
+     * string, and sent to send_rest_api_call
+     */
+
+
+    /* TODO: Step 4- Inside the "create_batch_list", a batch header is created, and signed, and
+     * then the received transactions array is put into a newly created 'Batch' object
+     */
+
+
+    /* TODO: Step 5- Inside the _send_request, use the url passed with --url, NOTE: For now, take
+     * it as "http://rest-api:8008"
+     */
+
 }
